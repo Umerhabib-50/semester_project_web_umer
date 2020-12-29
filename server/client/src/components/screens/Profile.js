9876cb9ae48791cf29480e3f3e1e57f231b05e20
Umerhabib-50 <71447236+Umerhabib-50@users.noreply.github.com>
2020-12-29 21:11:49 +0500

    working project files

---
+++ server/client/src/components/screens/Profile.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../../App";
+
+const Profile = () => {
+  const [mypics, setPics] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
+
+  useEffect(() => {
+    fetch("/mypost", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setPics(result.mypost);
+      });
+  }, []);
+  return (
+    <div style={{ maxWidth: "550px", margin: "0px auto" }}>
+      <div
+        style={{
+          margin: "18px 0px",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <div>
+            <img
+              style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+              src="https://images.unsplash.com/photo-1562813733-b31f71025d54?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1649&q=80"
+            />
+          </div>
+          <div>
+            <h4>{state ? state.name : "loading"}</h4>
+            <h5>{state ? state.email : "loading"}</h5>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "108%",
+              }}
+            >
+              <h6>20 posts</h6>
+              <h6>281 followers</h6>
+              <h6>157 following</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="file-field input-field" style={{ margin: "10px" }}>
+          <div className="btn #64b5f6 blue darken-1">
+            <span>Update pic</span>
+            <input type="file" />
+          </div>
+          <div className="file-path-wrapper">
+            <input className="file-path validate" type="text" />
+          </div>
+        </div>
+      </div>
+      <div className="gallery">
+        {mypics.map((item) => {
+          return (
+            <img
+              key={item._id}
+              className="item"
+              src={item.photo}
+              alt={item.title}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Profile;
